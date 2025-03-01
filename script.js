93320387525cbcb71f448ff35144d4dfa9df5bb2
Akib-dev1 <akibanj2017@gmail.com>
2025-03-01 12:17:21 +0600

    Index Html Work Dynamic Done

--- script.js
+++ script.js
@@ -4,6 +4,8 @@ const date=document.querySelector("#date");
 const colorChanger=document.querySelector("#color-changer");
 let valueCmpltCounter=parseInt(document.querySelector("#task-cmplt-counter").innerText);
 const completeBtns=document.querySelectorAll(".cmplt-btn");
+const historyBox=document.querySelector("#history-box");
+const clearBtn=document.querySelector("#clear");
 
 let now=new Date();
 day.innerText=now.toDateString().slice(0,3)+" ,";
@@ -28,5 +30,18 @@ for(let btn of completeBtns){
         }
         document.querySelector("#task-counter").innerText=taskCounter;
         document.querySelector("#task-cmplt-counter").innerText="0"+valueCmpltCounter;
+        historyBox.appendChild(historyDiv(event.target.closest(".parent").children[1].innerText));
     });
 }
+function historyDiv(title){
+    const div=document.createElement("div");
+    let latest=new Date();
+    const time=latest.toLocaleTimeString();
+    div.innerHTML=`<div class="bg-[#F4F7FF] p-3 mb-7 rounded-lg">
+                    <p>You have Complete The Task ${title} at ${time}</p>
+                </div>`;
+    return div;
+}
+clearBtn.addEventListener("click", function (event){
+    historyBox.innerHTML="";
+});
