3a722963125926259c23b302ce2ee00e66b76923
Akib-dev1 <akibanj2017@gmail.com>
2025-03-01 11:05:08 +0600

    Counter Part Done

--- script.js
+++ script.js
@@ -1,10 +1,12 @@
-const taskCounter=document.querySelector("#task-counter");
+let taskCounter=parseInt(document.querySelector("#task-counter").innerText);
 const day=document.querySelector("#day");
 const date=document.querySelector("#date");
 const colorChanger=document.querySelector("#color-changer");
+let valueCmpltCounter=parseInt(document.querySelector("#task-cmplt-counter").innerText);
+const completeBtns=document.querySelectorAll(".cmplt-btn");
 
 let now=new Date();
-day.innerText=now.toDateString().slice(0,3);
+day.innerText=now.toDateString().slice(0,3)+" ,";
 date.innerText=now.toDateString().slice(4,15);
 
 colorChanger.addEventListener("click",function (){
@@ -13,3 +15,18 @@ colorChanger.addEventListener("click",function (){
     let b=Math.floor(Math.random()*256);
     document.body.style.backgroundColor=`rgb(${r},${g},${b})`;
 });
+
+
+for(let btn of completeBtns){
+    btn.addEventListener("click",function (event){
+        alert("Board Updated Successfully!")
+        btn.disabled=true;
+        valueCmpltCounter--;
+        taskCounter++;
+        if(valueCmpltCounter===0){
+            alert("Congratulations!!! You have completed all the current task");
+        }
+        document.querySelector("#task-counter").innerText=taskCounter;
+        document.querySelector("#task-cmplt-counter").innerText="0"+valueCmpltCounter;
+    });
+}
